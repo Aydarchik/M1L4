2f3ddce6a87dfd824d7da05bd9cbe25f36e5e8a7
Aydarchik <126374145+Aydarchik@users.noreply.github.com>
2024-10-25 16:12:12 +0500

    Add files via upload

--- logic.py
+++ logic.py
@@ -11,7 +11,8 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-
+        self.hp = randint(10, 100)
+        self.power = randint(10,100)
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -33,14 +34,46 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
-        
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"""Имя твоего покеомона: {self.name}
+        Жизни: {self.power}
+        Урон: {self.power}"""
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
+class Wizard(Pokemon):
+    def info(self):
+        return "У тебя покемон-волшебник \n" + super().info()
 
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
+    def info(self):
+        return "У тебя покемон-боец \n" + super().info()   
+if __name__ == '__main__':
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
 
-
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
